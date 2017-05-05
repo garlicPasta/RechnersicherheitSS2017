d3a9a927b81b74d13fff0a3db45770cf0be42780
Jakob Krause <jakobkrause.mail@gmail.com>
2017-05-05 17:25:22 +0200

    Basic echo server with little loging

---
+++ ue2/ex3/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFFER_SIZE 1024
+#define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); fclose(logFile); exit(1); }
+
+static FILE *logFile;
+FILE * create_log_file(void);
+
+void log_message(char message[]);
+
+int main (int argc, char *argv[]) {
+    logFile = create_log_file();
+    if (argc < 2) on_error("Usage: %s [port]\n", argv[0]);
+    int port = atoi(argv[1]);
+
+
+    int server_fd, client_fd, err;
+    struct sockaddr_in server, client;
+    char buf[BUFFER_SIZE];
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) on_error("Could not create socket\n");
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int opt_val = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val);
+
+    err = bind(server_fd, (struct sockaddr *) &server, sizeof(server));
+    if (err < 0) on_error("Could not bind socket\n");
+
+    err = listen(server_fd, 128);
+    if (err < 0) on_error("Could not listen on socket\n");
+
+    printf("Server is listening on that nice %d\n", port);
+
+    while (1) {
+        socklen_t client_len = sizeof(client);
+        client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
+
+        log_message("Someone connected to the server");
+
+        if (client_fd < 0) on_error("Could not establish new connection\n");
+
+        while (1) {
+            int read = recv(client_fd, buf, BUFFER_SIZE, 0);
+            log_message("Someone write to the server");
+
+            if (!read) break; // done reading
+            if (read < 0) on_error("Client read failed\n");
+
+            err = send(client_fd, buf, read, 0);
+            if (err < 0) on_error("Client write failed\n");
+        }
+    }
+
+    fclose(logFile);
+    return 0;
+}
+
+void log_message(char message[]) {
+    fprintf(logFile, "%s\n", message);
+    fflush(logFile);
+}
+
+FILE * create_log_file() {
+    FILE *f = fopen("/var/log/ushoutd.log", "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    fprintf(f, "Hello i am a log file\n");
+    fflush(f);
+    return f;
+}
+
