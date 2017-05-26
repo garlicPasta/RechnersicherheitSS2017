024c670e05a1b946b276318a1a71bd049b1d47c9
Ramdane <ramdane@debianhost>
2017-05-25 23:09:25 -0400

    [ue5 - ex15] adds c program

---
+++ ue5/ex15/main.c
@@ -0,0 +1,19 @@
+int main () {
+
+	int authenticated = 0;
+	char buffer[9];
+
+	printf("Password:␣");
+
+	scanf("%s", buffer);
+
+	if (strcmp(buffer, "password") == 0)
+		authenticated = 1;
+	if (authenticated) {
+		printf("PPassword␣OK\n");
+		// do something else
+	} 
+	else 
+		printf("Access␣denied\n");
+	return 0;
+}
