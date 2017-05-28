9dc829951533d7b88a0811d78aabc4bc106f0f5e
Ramdane <ramdane@debianhost>
2017-05-27 22:12:13 -0400

    [ue5 - ex15] modifies main.c and adds objdump_output.txt

--- ue5/ex15/main.c
+++ ue5/ex15/main.c
@@ -3,17 +3,17 @@ int main () {
 	int authenticated = 0;
 	char buffer[9];
 
-	printf("Password:␣");
+	printf("Password:");
 
 	scanf("%s", buffer);
 
 	if (strcmp(buffer, "password") == 0)
 		authenticated = 1;
 	if (authenticated) {
-		printf("PPassword␣OK\n");
+		printf("Password OK\n");
 		// do something else
 	} 
 	else 
-		printf("Access␣denied\n");
+		printf("Access denied\n");
 	return 0;
 }
